41725112ef1476783aa870934306dbde52bd420a
srikanth <usaisrikanthuppalapati@gmail.com>
2025-07-16 23:17:25 +0530

    flask api day2

--- Flask API/getpost.py
+++ Flask API/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 '''This WSGI instance is our Web Server Gateway Interface app'''
 
 WSGI_app = Flask(__name__)
@@ -8,13 +8,16 @@ def welcome():
     return "Hi How r u? What r u dng?"
 
 # Rendering the html page 
-@WSGI_app.route("/index")
+@WSGI_app.route("/index", methods = ['GET'])
 def index():
     return render_template('index.html')
 
-@WSGI_app.route("/about")
-def about():
-    return render_template('index.html')
+@WSGI_app.route("/form", methods = ["GET", "POST"])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hi {name}'
+    return render_template('form.html')
 
 if __name__ == "__main__":
     WSGI_app.run(debug = True)
